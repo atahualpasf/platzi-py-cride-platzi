8a92f3cc19ef90041d70d1e5d0017658b699ef7c
Atahualpa Silva F <atahualpasilva@gmail.com>
2021-07-25 19:11:29 -0400

    feat: reestructuring project extensions

--- cride/taskapp/celery.py
+++ cride/taskapp/celery.py
@@ -1,27 +1,29 @@
 """Celery app config."""
 
 import os
-from celery import Celery
-from django.apps import apps, AppConfig
-from django.conf import settings
 
+from celery import Celery
+from django.apps import AppConfig, apps
+from django.conf import settings
 
 if not settings.configured:
     # set the default Django settings module for the 'celery' program.
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')  # pragma: no cover
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE", "config.settings.local"
+    )  # pragma: no cover
 
 
-app = Celery('cride')
+app = Celery("cride")
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object("django.conf:settings", namespace="CELERY")
 
 
 class CeleryAppConfig(AppConfig):
-    name = 'cride.taskapp'
-    verbose_name = 'Celery Config'
+    name = "cride.taskapp"
+    verbose_name = "Celery Config"
 
     def ready(self):
         installed_apps = [app_config.name for app_config in apps.get_app_configs()]
@@ -30,4 +32,4 @@ class CeleryAppConfig(AppConfig):
 
 @app.task(bind=True)
 def debug_task(self):
-    print(f'Request: {self.request!r}')  # pragma: no cover
+    print(f"Request: {self.request!r}")  # pragma: no cover
---
+++ cride/utils/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+
+
+class CRideModel(models.Model):
+    """Comparte Ride base model.
+
+    CRideModel acts as an abstract base class from which every
+    other model in the project will inherit. This class provides
+    every table with the folowwing attributes:
+        + created (DateTime): Store the datetime the object was created
+        + modified (DateTime): Store the last datetime the object was modified.
+    """
+
+    created = models.DateTimeField(
+        "created at",
+        auto_now_add=True,
+        help_text="Date time on which the object was created.",
+    )
+
+    modified = models.DateTimeField(
+        "modified at",
+        auto_now=True,
+        help_text="Date time on which the object was last modified.",
+    )
+
+    class Meta:
+        """Meta option."""
+
+        abstract = True
+
+        get_latest_by = "created"
+        ordering = ["-created", "-modified"]
+
+
+class Person(models.Model):
+    first_name = models.CharField()
+    last_name = models.CharField()
+
+
+class MyPerson(Person):
+    class Meta:
+        proxy = True
+
+    def say_hi(name):
+        pass
+
+
+MyPerson.objects.all()
+ricardo = MyPerson.objects.get(pk=1)
+ricardo.say_hi("Pablo")
+
+rulo = Person.objects.get(pk=2)
+rulo.say_hi("Pablo")
